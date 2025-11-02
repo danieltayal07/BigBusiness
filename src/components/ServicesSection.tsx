bffd0d7639f8df8ea01a63999bb268a81cf9dccf
danieltayal07 <tayaldaniel2@gmail.com>
2025-11-03 00:57:55 +0530

    check

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -79,7 +79,7 @@ export const ServicesSection = () => {
               </p>
               <Button 
                 variant="ghost" 
-                className="p-5 h-auto font-semibold text-primary group-hover:gap-2 transition-all"
+                className="p-1 h-auto font-semibold text-primary group-hover:gap-2 transition-all"
                 onClick={() => handleLearnMore(service.title)}
               >
                 Learn More
